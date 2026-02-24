93d59fb5ebf15a8cf19dfe87a4bf8a605d38136a
Alex Smith <asmith@porch.software>
2026-02-24 10:09:14 -0700

    basic structure

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { FwbHeading } from 'flowbite-vue';
+
+</script>
+
+<template>
+	<div class="w-full">
+		<FwbHeading tag="h1" class="text-center">Home</FwbHeading>
+	</div>
+</template>
---
+++ frontend/src/pages/InitiateExchange.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { FwbButton, FwbHeading, FwbInput } from 'flowbite-vue';
+import BarcodeIcon from '@/icons/BarcodeIcon.svg'
+import UserIcon from '@/icons/UserIcon.svg'
+import ProfileCardIcon from '@/icons/ProfileCardIcon.svg'
+
+</script>
+
+<template>
+	<div class="w-full">
+		<FwbHeading tag="h1" class="text-center mb-4">Initiate Broker Dealer Exchange</FwbHeading>
+
+		<div class="p-10 bg-[#f8f8f8] rounded-xl">
+			<div class="flex flex-wrap *:p-4 items-center">
+				<div class="w-1/3">
+					<FwbInput label="First Name">
+						<template #prefix>
+							<UserIcon />
+						</template>
+					</FwbInput>
+				</div>
+
+				<div class="w-1/3">
+					<FwbInput label="First Name">
+						<template #prefix>
+							<UserIcon />
+						</template>
+					</FwbInput>
+				</div>
+
+				<div class="w-1/3">
+					<FwbInput label="First Name">
+						<template #prefix>
+							<ProfileCardIcon />
+						</template>
+					</FwbInput>
+				</div>
+
+				<div class="w-1/3">
+					<FwbInput label="Contract Number">
+						<template #prefix>
+							<BarcodeIcon />
+						</template>
+					</FwbInput>
+				</div>
+
+				<div class="w-1/3">
+					<FwbButton color="default">Add Additional Contract</FwbButton>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
