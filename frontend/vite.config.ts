e92409a1de3d02a8f36255b914b5e68406e768d3
Alex Smith <asmith@porch.software>
2026-02-24 09:01:16 -0700

    copying fixes

---
+++ frontend/vite.config.ts
@@ -0,0 +1,35 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [
+		vue(),
+		vueDevTools(),
+		tailwindcss()
+	],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		},
+	},
+	server: {
+		host: true,
+		port: 5173,
+		watch: {
+			usePolling: true
+		},
+		proxy: {
+			'/api': {
+				target: 'https://jsonplaceholder.typicode.com',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ''), // Removes '/api' before sending to backend
+				secure: false,
+			}
+		}
+	}
+})
