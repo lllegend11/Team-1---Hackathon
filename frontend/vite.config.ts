3bc23ae04cf4f4efdbfe606f345b5cf4e8cc8a78
ViniRochaPacLife <vini.rocha@pacificlife.com>
2026-02-24 08:20:15 -0800

    amplify test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,35 +1,31 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		vueDevTools(),
-		tailwindcss()
-	],
+	plugins: [vue(), vueDevTools(), tailwindcss()],
 	resolve: {
 		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url))
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
 		},
 	},
 	server: {
 		host: true,
 		port: 5173,
 		watch: {
-			usePolling: true
+			usePolling: true,
 		},
 		proxy: {
-			'/api': {
-				target: 'https://jsonplaceholder.typicode.com',
+			"/api": {
+				target: "https://jsonplaceholder.typicode.com",
 				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, ''), // Removes '/api' before sending to backend
+				rewrite: (path) => path.replace(/^\/api/, ""), // Removes '/api' before sending to backend
 				secure: false,
-			}
-		}
-	}
-})
+			},
+		},
+	},
+});
